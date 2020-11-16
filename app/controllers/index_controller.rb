698190109163b2b30003e301a79cd5cff571125f
timazarenok <timofey.zarenok@gmail.com>
2020-11-16 11:48:33 +0300

    last review chnges

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,11 +1,5 @@
 class IndexController < ApplicationController
-    def index
-        #Country.create(name: "Belarus")
-        #Category.create(name: "Comedy")
-        #Director.create(full_name: "Test", biography: "rfdjknrejlfrefnm", date_of_birth: "19.06.2002", image: "rnffejnefdwed", country_id: Country.find_by(name: "Belarus").id)
-        #Movie.create(name: "Movie2", description: "renhfbejfhrenferjferghtrtytuytiu", clip: "clip_test2", release_date: "31.01.2021", duration: "02:03", category_id: Category.find_by(name: "Comedy").id, director_id: Director.find_by(full_name: "Test").id)
-        #Movie.create(name: "Movie3", description: "no desc", clip: "clip_test3", release_date: "26.11.2020", duration: "02:15", category_id: Category.find_by(name: "Comedy").id, director_id: Director.find_by(full_name: "Test").id)
-        #Movie.create(name: "Movie4", description: "gyuiopooiuuyuiop[ojiguk", clip: "clip_test4", release_date: "21.06.2020", duration: "01:30", category_id: Category.find_by(name: "Comedy").id, director_id: Director.find_by(full_name: "Test").id)
-        @movies = Movie.all.to_a
-    end
+  def index
+    @movies = Movie.all
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  def profile
-      
-  end
-  helper_method :after_sign_out_path_for
+  def profile; end
 end
